e4cebe024e4c5b3edc72c9ba4ea5ae3e7bb6e243
0x000000000019b6 <0x000000000019b6@gmail.com>
2022-11-10 16:30:44 +0100

    init

--- navicat_reset_trial.bat
+++ navicat_reset_trial.bat
@@ -6,6 +6,11 @@ for /f %%i in ('"REG QUERY "HKEY_CURRENT_USER\Software\PremiumSoft\Navicat" /s |
 )
 echo.
 
+for /f %%i in ('"REG QUERY "HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPG" /s | findstr /L Registration"') do (
+	echo Delete Registration: %%i
+    reg delete %%i /va /f
+)
+echo.
 
 for /f %%i in ('"REG QUERY "HKEY_CURRENT_USER\Software\Classes\CLSID" /s | findstr /E Info"') do (
 	echo Delete Info folder under: %%i
